2fc155cb1c9ed4435ab0f21b07fa28a863278a90
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-28 22:02:37 -0600

    Ejercicio3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ejercicio 3
+
+
+Console.Write("Por favor, ingrese su fecha de cumpleaños (DD-MM): ");
+if (DateTime.TryParse(Console.ReadLine(), out DateTime FechaCumple))
+{
+    DateTime FechaActual = DateTime.Today;
+
+    if (FechaCumple < FechaActual)
+    {
+        FechaCumple = FechaCumple.AddYears(1);
+    }
+
+    TimeSpan TiempoRes = FechaCumple - FechaActual;
+
+    Console.WriteLine($"Su próximo cumpleaños es el {FechaCumple.ToString("dd-MM-yyyy")}.");
+
+    Console.WriteLine($"Faltan {TiempoRes.Days} días hasta su próximo cumpleaños.");
+}
+else
+{
+    Console.WriteLine("Fecha ingresada no válida. Por favor, utilice el formato DD-MM.");
+}
