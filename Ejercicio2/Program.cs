dca59b981c74c0a7a80bab77c364bf7f67171fbe
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-28 21:44:41 -0600

    Ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ejercicio2
+
+double prestamo = 3000; 
+int plazoMeses = 6;    
+DateTime fechaActual = DateTime.Now;
+
+Console.WriteLine($"Fecha de inicio del préstamo: {fechaActual.ToString("dd-MM-yyyy")}");
+Console.WriteLine($"Monto del préstamo: {prestamo}$");
+Console.WriteLine($"Plazo del préstamo: {plazoMeses} meses");
+Console.WriteLine("\nFechas de pago y cuotas:");
+
+double cuota = prestamo / plazoMeses;
+
+for (int i = 1; i <= plazoMeses; i++)
+{
+    fechaActual = fechaActual.AddMonths(1);
+    Console.WriteLine($"{fechaActual.ToString("dd-MM-yyyy")}: ${cuota.ToString("0.00")}");
+}
