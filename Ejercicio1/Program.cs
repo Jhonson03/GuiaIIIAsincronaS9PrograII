fb0ceeffeda86a196ca8eb5df6e2eaebcbc287ec
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-28 21:31:24 -0600

    Ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Ejercicio 1
+
+//Por algunos entornos que no reconocen la ñ le puse years
+
+int[] years = new int[] { 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 };
+
+foreach (int i in years)
+{
+    bool Bisiesto = DateTime.IsLeapYear(i);
+
+    Console.WriteLine($"{i} {(Bisiesto ? "es" : "no es")} bisiesto");
+}
